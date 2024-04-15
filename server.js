a8971c354d0d07a2250b2221bf20aad77e142b00
kirkyky123 <131261278+kirkyky123@users.noreply.github.com>
2024-04-15 09:00:01 -0400

    done

--- server.js
+++ server.js
@@ -11,15 +11,13 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-const craftData = fs.readFileSync("crafts.json", "utf8");
-const crafts = JSON.parse(craftData);
-
-mongoose.connect("mongodb+srv://chhetrik:PrBBYtadSqegqBwl@cluster0.mrtzbch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose
+  .connect("mongodb+srv://chhetrik:PrBBYtadSqegqBwl@cluster0.mrtzbch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Connected to MongoDB"))
   .catch(error => console.error("Could not connect to MongoDB", error));
 
 const craftSchema = new mongoose.Schema({
-  _id:mongoose.SchemaTypes.ObjectId,
+  // _id:mongoose.SchemaTypes.ObjectId,
   name: String,
   description: String,
   supplies: [String],
@@ -27,23 +25,6 @@ const craftSchema = new mongoose.Schema({
 });
 
 const Craft = mongoose.model("Craft", craftSchema);
-
-const schema = new mongoose.Schema({
-  name: String,
-});
-
-async function createMessage() {
-  const result = await message.save();
-  console.log(result);
-}
-
-//this creates a Message class in our app
-const Message = mongoose.model("Message", schema);
-const message = new Message({
-  name: "Hello testtstst",
-});
-
-createMessage();
 
 
 app.get("/", (req, res) => {
@@ -62,19 +43,23 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.get("/api/crafts", (req, res) => {
-  res.send(crafts);
+  getCrafts(res);
 });
+
+const getCrafts = async (res) => {
+  const crafts = await Craft.find();
+  res.send(crafts);
+};
+
 
 app.get("/api/crafts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const craft = crafts.find((c) => c._id === id);
-
-  if (!craft){
-    return res.status(404).send("The craft with the given id was not found");
-  }
-
-  res.send(craft);
+  getCraft(res, req.params.id);
 });
+
+const getCraft = async (res) => {
+  const craft = await Craft.findOne({ _id: id });
+  res.send(craft);
+};
 
 app.post("/api/crafts", upload.single("img"), (req, res) => {
   const result = validateCraft(req.body);
@@ -84,26 +69,25 @@ app.post("/api/crafts", upload.single("img"), (req, res) => {
     return;
   }
 
-  const craft = {
-    _id: crafts.length + 1,
+  const craft = new Craft({
     name: req.body.name,
     description: req.body.description,
-    supplies: req.body.supplies.split(","),
-  };
+    supplies: req.body.supplies.split(",")
+  })
 
   if (req.file) {
     craft.image = req.file.filename;
   }
 
-  crafts.push(craft);
-  res.send(crafts);
-  fs.writeFileSync("crafts.json", JSON.stringify(crafts, null, 2));
+  createCraft(res, craft);
 });
 
-app.put("/api/crafts/:id", upload.single("img"), (req, res)=>{
-  const craft = crafts.find((c)=>c._id === parseInt(req.params.id));
+const createCraft = async (res, craft) =>{
+  const result = await craft.save();
+  res.send(craft);
+}
 
-  if(!craft) res.status(400).send("Craft with given id was not found");
+app.put("/api/crafts/:id", upload.single("img"), (req, res)=>{
 
   const result = validateCraft(req.body);
 
@@ -112,31 +96,33 @@ app.put("/api/crafts/:id", upload.single("img"), (req, res)=>{
     return;
   }
 
-  craft.name = req.body.name;
-  if(req.file) {
-    craft.image = req.file.filename;
-  }
-  craft.description - req.body.description;
-  craft.supplies = req.body.supplies.split(",");
+  updateCraft(req, res);
 
-  res.send(craft);
-  fs.writeFileSync("crafts.json", JSON.stringify(crafts, null, 2));
 });
+
+const updateCraft = async(req, res) => {
+  let fieldsToUpdate = {
+    name: req.body.name,
+    description: req.body.description,
+    supplies: req.body.supplies.split(",")
+  }
+
+  if(req.file){
+    fieldsToUpdate.img = "images/" + req.file.filename;
+  }
+
+  const result = await Craft.updateOne({_id:req.params.id}, fieldsToUpdate);
+  res.send(result);
+}
 
 app.delete("/api/crafts/:id", (req, res)=>{
-  const id = parseInt(req.params.id);
-
-  const craft = crafts.find((c)=>c._id === id);
-
-  if(!craft){
-    res.status(404).send("The craft with the given id was not found");
-    return;
-  }
-
-  const index = crafts.indexOf(craft);
-  crafts.splice(index,1);
-  fs.writeFileSync("crafts.json", JSON.stringify(crafts, null, 2));
+  removeCraft(res, req.params.id);
 });
+
+const removeCraft = async(res, id) => {
+  const craft = await Craft.findByIdAndDelete(id);
+  res.send(craft);
+}
 
 const validateCraft = (craft) => {
   const schema = Joi.object({
